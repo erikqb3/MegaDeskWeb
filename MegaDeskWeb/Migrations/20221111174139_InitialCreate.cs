682721e117f62bc13a59a2e60dc493d43e99ad9c
erikqb3 <erikqb3@gmail.com>
2022-11-11 10:45:33 -0700

    database works

---
+++ MegaDeskWeb/Migrations/20221111174139_InitialCreate.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MegaDeskWeb.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Material",
+                columns: table => new
+                {
+                    MaterialId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<int>(type: "INTEGER", nullable: false),
+                    price = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Material", x => x.MaterialId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RushOption",
+                columns: table => new
+                {
+                    RushOptionId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    days = table.Column<int>(type: "INTEGER", nullable: false),
+                    calcPrice = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RushOption", x => x.RushOptionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Desk",
+                columns: table => new
+                {
+                    DeskId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    width = table.Column<int>(type: "INTEGER", nullable: false),
+                    depth = table.Column<int>(type: "INTEGER", nullable: false),
+                    drawerCount = table.Column<int>(type: "INTEGER", nullable: false),
+                    MaterialId = table.Column<int>(type: "INTEGER", nullable: false),
+                    RushOptionId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Desk", x => x.DeskId);
+                    table.ForeignKey(
+                        name: "FK_Desk_Material_MaterialId",
+                        column: x => x.MaterialId,
+                        principalTable: "Material",
+                        principalColumn: "MaterialId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Desk_RushOption_RushOptionId",
+                        column: x => x.RushOptionId,
+                        principalTable: "RushOption",
+                        principalColumn: "RushOptionId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DeskQuote",
+                columns: table => new
+                {
+                    DeskQuoteId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    customerName = table.Column<string>(type: "TEXT", nullable: false),
+                    quoteTotalPrice = table.Column<float>(type: "REAL", nullable: false),
+                    startDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    finishDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    contactMethod = table.Column<string>(type: "TEXT", nullable: false),
+                    contactInfo = table.Column<string>(type: "TEXT", nullable: false),
+                    DeskId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeskQuote", x => x.DeskQuoteId);
+                    table.ForeignKey(
+                        name: "FK_DeskQuote_Desk_DeskId",
+                        column: x => x.DeskId,
+                        principalTable: "Desk",
+                        principalColumn: "DeskId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Desk_MaterialId",
+                table: "Desk",
+                column: "MaterialId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Desk_RushOptionId",
+                table: "Desk",
+                column: "RushOptionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeskQuote_DeskId",
+                table: "DeskQuote",
+                column: "DeskId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DeskQuote");
+
+            migrationBuilder.DropTable(
+                name: "Desk");
+
+            migrationBuilder.DropTable(
+                name: "Material");
+
+            migrationBuilder.DropTable(
+                name: "RushOption");
+        }
+    }
+}
