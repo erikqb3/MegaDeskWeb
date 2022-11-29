9f6e64b207329bc6a52e56a57a2d5522e9c17f42
erikqb3 <erikqb3@gmail.com>
2022-11-29 10:43:34 -0700

    updated

--- MegaDeskWeb/Migrations/20221129174110_resetDBAgain.Designer.cs
+++ MegaDeskWeb/Migrations/20221129174110_resetDBAgain.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MegaDeskWeb.Migrations
 {
     [DbContext(typeof(MegaDeskWebContext))]
-    partial class MegaDeskWebContextModelSnapshot : ModelSnapshot
+    [Migration("20221129174110_resetDBAgain")]
+    partial class resetDBAgain
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.10");
---
+++ MegaDeskWeb/Migrations/20221129174110_resetDBAgain.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MegaDeskWeb.Migrations
+{
+    public partial class resetDBAgain : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "price",
+                table: "Material",
+                type: "decimal(18, 2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "quoteTotalPrice",
+                table: "DeskQuote",
+                type: "decimal(18, 2)",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "REAL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "price",
+                table: "Material",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "decimal(18, 2)");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "quoteTotalPrice",
+                table: "DeskQuote",
+                type: "REAL",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "decimal(18, 2)");
+        }
+    }
+}
